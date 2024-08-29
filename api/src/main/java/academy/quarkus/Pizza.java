d8c0d75677efd24868dcca299c0866d93c8d1afa
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-08-29 16:18:00 +0000

    S01E02

---
+++ api/src/main/java/academy/quarkus/Pizza.java
@@ -0,0 +1,13 @@
+package academy.quarkus;
+
+public class Pizza {
+    String description;
+
+    public Pizza(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
