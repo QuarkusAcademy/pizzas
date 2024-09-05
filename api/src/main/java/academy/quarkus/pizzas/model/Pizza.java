34e7ff9dfb3f9362aff635d6853b5eacd2baa839
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-09-05 13:54:08 +0200

    wip

--- api/src/main/java/academy/quarkus/pizzas/model/Pizza.java
+++ api/src/main/java/academy/quarkus/pizzas/model/Pizza.java
@@ -2,19 +2,10 @@ package academy.quarkus.pizzas.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
-import jakarta.transaction.Transactional;
 
 @Entity
 public class Pizza extends PanacheEntity {
     String description;
-
-    @Transactional
-    public static Pizza persist(String description){
-        var pizza = new Pizza();
-        pizza.description = description;
-        pizza.persist();
-        return pizza;
-    }
 
     public Pizza(){}
 
@@ -25,6 +16,4 @@ public class Pizza extends PanacheEntity {
     public String getDescription() {
         return description;
     }
-
-
 }
