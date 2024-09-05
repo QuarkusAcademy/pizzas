3128a3997032e3be2c8d58aa1ecf8aa2f3baa249
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-09-05 08:30:05 +0000

    Main class added

---
+++ api/src/main/java/academy/quarkus/Main.java
@@ -0,0 +1,13 @@
+package academy.quarkus;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String... args){
+        Quarkus.run(args);
+    }
+
+}
